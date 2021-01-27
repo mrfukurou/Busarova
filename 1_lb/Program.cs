c02b00f775ab018d98ebdcce60b1a846375ef0b2
fukur <fukur@LAPTOP-7EQMGM6H>
2021-01-27 18:39:11 +0300

    Добавьте файлы проекта.

---
+++ 1_lb/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите сторону треугольника a= ");
+            float a = float.Parse(Console.ReadLine());
+            double r = a / (2 * Math.Sqrt(3));
+            Console.WriteLine("Радиус вписанной окружности r=" + r);
+
+        }
+    }
+}
